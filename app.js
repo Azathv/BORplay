9fa1f165b4826373fca887a0fc13400cb397455c
Azat Khadiev <72373959+Azathv@users.noreply.github.com>
2025-11-08 01:46:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Upload tugmasi
 document.getElementById("uploadBtn").addEventListener("click", () => {
-  alert("📤 Qo‘shiq yuklash funksiyasi tez orada qo‘shiladi!");
+  alert("📤 Fayl yuklash funksiyasi tez orada qo‘shiladi!");
 });
 
-// Eye (visibility) tugmasi
+// Visibility toggle
 const visibilityBtn = document.getElementById("visibilityToggle");
 const eyeIcon = document.getElementById("eyeIcon");
 
@@ -11,15 +11,16 @@ let isVisible = true;
 
 visibilityBtn.addEventListener("click", () => {
   isVisible = !isVisible;
+
   if (isVisible) {
     eyeIcon.classList.remove("fa-eye-slash");
     eyeIcon.classList.add("fa-eye");
-    visibilityBtn.classList.remove("hidden");
-    visibilityBtn.title = "Ko‘rinish: yoqilgan";
+    visibilityBtn.classList.remove("off");
+    visibilityBtn.title = "Ko‘rinish yoqilgan";
   } else {
     eyeIcon.classList.remove("fa-eye");
     eyeIcon.classList.add("fa-eye-slash");
-    visibilityBtn.classList.add("hidden");
-    visibilityBtn.title = "Ko‘rinish: o‘chirilgan";
+    visibilityBtn.classList.add("off");
+    visibilityBtn.title = "Ko‘rinish o‘chirilgan";
   }
 });
