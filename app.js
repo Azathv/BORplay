a10f40c42ee9e199f6b27e8a9c24746e6d7f13ba
Azat Khadiev <72373959+Azathv@users.noreply.github.com>
2025-11-08 16:25:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-const SUPABASE_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://xeidlpkwwpghtlzocjjh.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlaWRscGt3d3BnaHRsem9jampoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwMDY0MjQsImV4cCI6MjA3NzU4MjQyNH0.40V9blptMQ9pTU8kHAqtlyaduoFevOO12uW563WTToI";
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const uploadBtn = document.getElementById("uploadBtn");
